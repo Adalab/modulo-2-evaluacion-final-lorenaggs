e878fea2b82678e82770d3efb134d8dd036eb48b
Lorena Guartazaca <lorenaggs2812@gmail.com>
2022-04-10 08:51:57 -0500

    add favorites

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,9 @@
 
 const buttonSearch = document.querySelector('.button-search');
 const cocktailssearch = document.querySelector('.cocktailssearch');
+// const listcocktails = document.querySelector('.listcocktails');
 
-let html = [];
+let data = [];
 
 function handleClick() {
   const input = document.querySelector('.input').value;
@@ -12,46 +13,122 @@ function handleClick() {
       return response.json();
     })
     .then((data) => {
-      let html = '';
-      html = `Lista de cocteles buscados`;
       const dataDrinks = data.drinks;
       if (data.drinks === null) {
-        html = `El coctel buscado no existe`;
+        cocktailssearch.innerHTML = `El coctel buscado no existe`;
       } else {
-        for (const drink of dataDrinks) {
-          const nameDrink = drink.strDrink;
-          const imageDrink = drink.strDrinkThumb;
-          const idDrink = drink.idDrink;
-
-          html += `<li>`;
-          html += `<div class="cocktails" id="${idDrink}" onclick='favorite(${nameDrink})'> ${nameDrink}`;
-          if ((imageDrink === '')) {
-            html += `<img class="image" src="https://via.placeholder.com/210x295/ffffff/666666/?text=C%C3%B3ctel" width="50">`;
-          } else {
-            html += `<img class="image" src="${imageDrink}" width="50">`;
-          }
-          html += `</div>`;
-          html += `</li>`;
-        }
+        localStorage.setItem('drinkslocal', JSON.stringify(dataDrinks));
+        buildCoctails();
       }
-      cocktailssearch.innerHTML = html;
     });
 }
 
 buttonSearch.addEventListener('click', handleClick);
 
+function buildCoctails() {
+  const drinkslocal = JSON.parse(localStorage.getItem('drinkslocal'));
+  let html = '';
+  html = `<h3 class="licoctails">Lista de cocteles buscados</h3>`;
+  // html = `<div>`;
+  for (const drink of drinkslocal) {
+    const nameDrink = drink.strDrink;
+    const imageDrink = drink.strDrinkThumb;
+    const idDrink = drink.idDrink;
 
-// paletas favoritas
-const favorites = document.querySelector('.favorites');
-const listcocktails = document.querySelector('.listcocktails');
+    html += `<li class="listsearch">`;
+    html += `<div class="cocktails" id="${idDrink}"> ${nameDrink}`;
+    if (imageDrink === '') {
+      html += `<img class="image" src="https://via.placeholder.com/210x295/ffffff/666666/?text=C%C3%B3ctel" width="50">`;
+    } else {
+      html += `<img class="image" src="${imageDrink}" width="50">`;
+    }
+    html += `</div>`;
+    html += `</li>`;
+  }
+  // html = `</div>`;
+  cocktailssearch.innerHTML = html;
+  handleClickCocktails();
+}
 
+// let favorites = [];
+function handleClickCocktails() {
+  const cocktails = document.querySelectorAll('.cocktails');
+  for (const item of cocktails) {
+    item.addEventListener('click', addFavorite);
+  }
+}
+let favorites = [];
+function addFavorite(event) {
+  const drinkslocal = JSON.parse(localStorage.getItem('drinkslocal'));
+  const favoritesHtml = document.querySelector('.favorites');
+  const idCocktailsSelect = event.currentTarget.id;
+  const favoriteSelect = drinkslocal.find(
+    (x) => x.idDrink === idCocktailsSelect
+  );
+  favorites.push(favoriteSelect);
+  // localstorage
+  localStorage.setItem('drinkslocalFav', JSON.stringify(favorites));
 
-// function favorite(event) {
+  let favHtml = '';
 
-//   console.log(event.currentTarget.idDrink);
-//   console.log('hola');
-// }
+  const nameDrink = favoriteSelect.strDrink;
+  const imageDrink = favoriteSelect.strDrinkThumb;
+  const idDrink = favoriteSelect.idDrink;
+  favHtml += `<li class="favorites">`;
+  favHtml += `<div class="cocktailsfavorites"> ${nameDrink}`;
+  favHtml += `<img class="image" src="${imageDrink}" width="50">`;
+  favHtml += `<p class="selecdelete" id="fav-${idDrink}">x</p>`;
+  favHtml += `</div>`;
+  favHtml += `</li>`;
 
+  favoritesHtml.innerHTML += favHtml;
+  colorSelecFav(idDrink);
+  handleClickDelete();
+}
 
+function colorSelecFav(idDrink) {
+  const cocktailsFav = document.getElementById(idDrink);
+  cocktailsFav.classList.add('selecfavo');
+}
 
-// listcocktails.addEventListener('click', handleClickFavorite);
+function handleClickDelete() {
+  const selecdelete = document.querySelectorAll('.selecdelete');
+  for (const itemFav of selecdelete) {
+    itemFav.addEventListener('click', deleteFavorite);
+  }
+}
+
+function deleteFavorite(event) {
+  const idFavorite = event.currentTarget.id;
+  // console.log(idFavorite);
+
+  let idCoctel = idFavorite.split('-')[1];
+  console.log(idCoctel);
+  const drinkslocalFav = JSON.parse(localStorage.getItem('drinkslocalFav'));
+  const indexFavorite = drinkslocalFav.findIndex((x) => {
+    // console.log(typeof x.idDrink, typeof idCoctel);
+    return x.idDrink === idCoctel;
+  });
+  drinkslocalFav.splice(indexFavorite, 1);
+
+  localStorage.setItem('drinkslocalFav', JSON.stringify(drinkslocalFav));
+  const favoritesHtml = document.querySelector('.favorites');
+  favoritesHtml.innerHTML = '';
+  let favHtml = '';
+  for (const drinks of drinkslocalFav) {
+    const nameDrink = drinks.strDrink;
+    const imageDrink = drinks.strDrinkThumb;
+    const idDrink = drinks.idDrink;
+    favHtml += `<li class="favorites">`;
+    favHtml += `<div class="cocktailsfavorites"> ${nameDrink}`;
+    favHtml += `<img class="image" src="${imageDrink}" width="50">`;
+    favHtml += `<p class="selecdelete" id="fav-${idDrink}">x</p>`;
+    favHtml += `</div>`;
+    favHtml += `</li>`;
+
+    favoritesHtml.innerHTML += favHtml;
+    colorSelecFav(idDrink);
+  }
+
+  handleClickDelete();
+}
